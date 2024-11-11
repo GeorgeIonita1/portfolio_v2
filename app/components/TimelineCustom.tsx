123fd03facabdfe476f7f65326cd40bf839977b8
george_wsl <g3orrg301@gmail.com>
2024-11-11 23:17:58 +0200

    Add bento grid for the timeline. Dummy content

--- app/components/TimelineCustom.tsx
+++ app/components/TimelineCustom.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import { Timeline } from "@/app/components/ui/timeline";
+import { BentoGridCustom } from "./BentoGridCustom";
 
 export function TimelineCustom() {
     const data = [
         {
-            title: "2024",
+            title: "Early 2024",
             content: (
                 <div>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-                        Built and launched Aceternity UI and Aceternity UI Pro from scratch
-                    </p>
+                    <TimelineSectionRole>
+                        Web Devaloper - Finastra
+                    </TimelineSectionRole>
+                    <TimelineSectionTitle>
+                        Joined an amazing team with an awesome company culture which gave me the opportunity to further grow and emphasize my soft skills.
+                    </TimelineSectionTitle>
+
                     <div className="grid grid-cols-2 gap-4">
                         <Image
                             src="/Avatar_sample.jpg"
@@ -45,47 +50,17 @@ export function TimelineCustom() {
             ),
         },
         {
-            title: "Early 2023",
+            title: "2023",
             content: (
                 <div>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-                        I usually run out of copy, but when I see content this big, I try to
-                        integrate lorem ipsum.
-                    </p>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-                        Lorem ipsum is for people who are too lazy to write copy. But we are
-                        not. Here are some more example of beautiful designs I built.
-                    </p>
-                    <div className="grid grid-cols-2 gap-4">
-                        <Image
-                            src="/Avatar_sample.jpg"
-                            alt="hero template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                        <Image
-                            src="/Avatar_sample.jpg"
-                            alt="feature template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                        <Image
-                            src="/Avatar_sample.jpg"
-                            alt="bento template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                        <Image
-                            src="/Avatar_sample.jpg"
-                            alt="cards template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                    </div>
+                    <TimelineSectionRole>
+                        Front-end Web Devaloper - Tremend Software Consulting
+                    </TimelineSectionRole>
+                    <TimelineSectionTitle>
+                        Proud to be involved in three major projects and deliver quality and scalable code. Here I developed strong and valuable technical skills.
+                    </TimelineSectionTitle>
+
+                    <BentoGridCustom />
                 </div>
             ),
         },
@@ -151,5 +126,21 @@ export function TimelineCustom() {
         <div className="w-full">
             <Timeline data={data} />
         </div>
+    );
+}
+
+function TimelineSectionTitle({ children }: { children: string }) {
+    return (
+        <h4 className="text-neutral-800 dark:text-neutral-200 text-md md:text-lg font-normal mb-10">
+            {children}
+        </h4>
+    );
+}
+
+function TimelineSectionRole({ children }: { children: string }) {
+    return (
+        <h3 className="text-neutral-800 dark:text-neutral-200 text-lg md:text-xl font-bold mb-2">
+            {children}
+        </h3>
     );
 }
